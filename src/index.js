202fb718ff774f6dedb2492bd726021db7c945c2
sameeksha <sameeksha140404@gmail.com>
2025-11-29 21:08:49 +0530

    Added body Parser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const ServerConfig = require('./config/serverConfig');
 const { connect } = require('mongoose');
@@ -7,12 +9,33 @@ const connectDB = require('./config/dbConfig');
 
 
 const app= express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));            
+app.use(bodyParser.text());
 // here app is the server 
+
+
+app.post('/ping',(req,res)=>{
+    console.log(req.body);
+    return res.json({message:"pong"});
+});
+
+
 app.listen(ServerConfig.PORT,async ()=>{
     await connectDB();
     console.log(`Server is running on http://localhost:${ServerConfig.PORT}`);
    
 });
+
+
+
+// req from client->routing layer->controller-><-servicer layer-><-connected to the schema repository-><-databases
+//  this is the basic architecture  mvc model
+// each layer has its own responsibility and it helps to maintain the code and also helps to understand the flow
+// service layer,schema(persistance and db file) and repository(dao)is all collectively called model.
+// of the flow of the diagram.
+
 
 //yz1evu8J5oYJ0xx8 password of mongodb atlas
 //sameeksha140404_db_user 
